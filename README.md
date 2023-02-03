0a025e4a4da7b019ce78a1026012151a02478427
jkrieger6 <jkrieger11@gmail.com>
2023-02-03 11:31:38 -0800

    updated project README file

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# prework-study-guide-real
-bootcamp prework study guide
+# Prework Study Guide Webpage
+## Description
+To have an organized webpage with all of the prework study materials
+To better understand the languages of coding and develop tools to help excell in the bootcamp
+## Installation
+N/A
+## Usage
+Each section is divided into the main topics of the prework and below each topic lsited are the main lessons that were gathered. Each section are organized onto one page with the subjects of the topics listed below. Along with the prework information you can also open op DevTools on the console and see the JavaScript running. From there you can choose a topic to study as well as see a list of the topics studied. 
+## Credits
+N/A
+## License
+Please refer to license in the repo.
+
--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+<<<<<<< HEAD
 for(var x=0; x < topics.length; x++){
 }
 /*
@@ -9,12 +10,38 @@ if (topic === 'HTML'); {
 } else if (topic === 'Git') {
     console.log("Let's study Git!");
 }else if (topic === "JavaScript") {
+=======
+var randomTopic = topics[Math.floor(Math.random() * topics.length)];
+function listTopics() {
+    for (var x=0; x< topics.length; x++) {
+        console.log(topics[x]);
+    }
+}
+for(var x=0; x < topics.length; x++){
+}
+function selectTopic() {
+if (randonmTopic === 'HTML'); {
+    console.log("Let's study HTML!");
+} else if (randomTopic === 'CSS') {
+    console.log("Let's study CSS!");
+} else if (randomTopic === 'Git') {
+    console.log("Let's study Git!");
+}else if (randomTopic === "JavaScript") {
+>>>>>>> 0d3959be6ea6727273677baaf28526ce2a5e6aff
     console.log("Let's study JavaScript!");
 } else {
     console.log('Please try again!');
 }
+<<<<<<< HEAD
 */
 var shapes = ["triangle", "square", "pentagon", "circle"];
 for(var x = 0; x < shapes.length; x++) {
     console.log(shapes[x]);
 }
+=======
+}
+console.log('Here are the topics we learned through Prework');
+listTopics();
+console.log('Which topic should we study first?');
+selectTopic();
+>>>>>>> 0d3959be6ea6727273677baaf28526ce2a5e6aff
